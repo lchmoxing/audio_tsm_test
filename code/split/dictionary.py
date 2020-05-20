749949ec22230157ba6ccebee3711959433e598a
lchmoxing <lchao@zju.edu.cn>
2020-05-20 16:55:40 +0800

    dic

--- code/split/dictionary.py
+++ code/split/dictionary.py
@@ -1,4 +1,15 @@
+import os
+import sys
 import cmudict
+import pandas as pd
+
+
+path = os.path.dirname(os.path.abspath('..'))
+text_path = path + '/dataset/text/textwithwakewords.txt'
+print(text_path)
+all_txt = pd.read_table(text_path,header=None)
+print(all_txt)
+
 
 prondict = cmudict.dict()
 result = prondict['speech']
