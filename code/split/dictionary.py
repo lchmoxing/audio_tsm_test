46bd81c0444aabfbdb5c2aa101834423491cfac0
lchmoxing <lchao@zju.edu.cn>
2020-05-22 17:10:34 +0800

    1

--- code/split/dictionary.py
+++ code/split/dictionary.py
@@ -1,16 +1,24 @@
 import os
-import sys
+import nltk
 import cmudict
+import ssl
 import pandas as pd
-
-
-path = os.path.dirname(os.path.abspath('..'))
-text_path = path + '/dataset/text/textwithwakewords.txt'
-print(text_path)
-all_txt = pd.read_table(text_path,header=None)
-print(all_txt)
-
+nltk.download('punkt')
 
 prondict = cmudict.dict()
-result = prondict['speech']
+path = os.path.dirname(os.path.abspath('..'))
+text_path = path + '/dataset/text/textwithwakewords.txt'
+
+all_txt = pd.read_table(text_path,header=None)
+#print(all_txt)
+
+#tokenize = lambda x: nltk.word_tokenize[x]
+#all_txt['split'] = all_txt(tokenize)
+#print(all_txt)
+
+
+#result = prondict['speech']
+teset_txt =
+result = nltk.word_tokenize('Okay Google, navigate to my home.')
+print(type(result))
 print(result)
