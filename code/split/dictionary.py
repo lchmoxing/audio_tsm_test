819c5005ef638e29b7622c5dc656cfc723792fc6
lchmoxing <lchao@zju.edu.cn>
2020-05-23 10:20:27 +0800

    split

--- code/split/dictionary.py
+++ code/split/dictionary.py
@@ -15,17 +15,12 @@ all_txt = pd.read_table(text_path,header=None)
 #tokenize = lambda x: nltk.word_tokenize[x]
 #all_txt['split'] = all_txt(tokenize)
 #print(all_txt)
-
-
 #result = prondict['speech']
 test_txt = 'Okay Google, navigate to my home'
 test_txt_revise = test_txt.replace(",",'')
 split_result =  nltk.word_tokenize(test_txt_revise)
 print(split_result)
 for i in range(len(split_result)):
-    split_result[i] =  prondict[split_result[i]]
+    split_result[i] =  prondict[split_result[i].lower()]
 
 print(split_result)
-#result = nltk.word_tokenize('Okay Google, navigate to my home.')
-#print(type(result))
-#print(result)
