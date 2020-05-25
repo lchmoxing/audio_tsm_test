578ea3ae609f91398a5ab4352fe4835b5ab2d0af
lchmoxing <lchao@zju.edu.cn>
2020-05-25 15:03:32 +0800

    1

--- code/split/dictionary_table.py
+++ code/split/dictionary_table.py
@@ -5,8 +5,11 @@ import nltk
 
 nltk.download('punkt')
 prondict = cmudict.dict()
-path = os.path.dirname(os.path.abspath('..'))
-text_path = path + '/dataset/speech_origin/text/textwithwakewords.txt'
+path = os.path.dirname(os.path.abspath('.'))
+
+text_path = path + '/ASR/baidu/baidu_with_wake_words0.csv'
+
+#F:\github\audio_tsm_test\code\ASR\baidu
 
 def split_tran(txt):
     txt_revise = txt.replace(",", '').replace(".", '')
@@ -15,21 +18,15 @@ def split_tran(txt):
         split_result[i] = prondict[split_result[i].lower()]
     return split_result
 
-all_txt = pd.read_table(text_path, header=None)
-all_txt.columns = ['origin']
 
+
+all_txt = pd.read_table(text_path)
+#all_txt.columns = ['origin']
+print(all_txt)
+'''
 tran = []
 for index, row in all_txt.iterrows():
     tran.append(split_tran(row['origin']))
 all_txt['tran'] = tran
 all_txt.to_excel("split_tran.xlsx")
-
-'''
-test_txt = 'Okay Google, navigate to my home'
-test_txt_revise = test_txt.replace(",",'')
-split_result =  nltk.word_tokenize(test_txt_revise)
-print(split_result)
-for i in range(len(split_result)):
-    split_result[i] =  prondict[split_result[i].lower()]
-print(split_result)
 '''
