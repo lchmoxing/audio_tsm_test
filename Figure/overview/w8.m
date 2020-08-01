0d34049a5f57a7dca5ba15553fd6a5714f47f6f0
lchmoxing <lchmoxing@gmail.com>
2020-08-01 20:48:06 +0800

    fig

--- Figure/overview/w8.m
+++ Figure/overview/w8.m
@@ -1,11 +1,12 @@
-[sampledata,FS] = audioread('take_action.wav');
-x = 1:1:26000;
-y = sampledata(700:1:21700);
+clear all;
+[sampledata,FS] = audioread('11.wav');
+x = 1:1:32768;
+y = sampledata(1:1:32768);
 plot(y)
 
 set(gca,'fontsize',20);
 
-xlim([0,21000]);
+xlim([0,32768]);
 xbounds = xlim();
 set(gca, 'xtick', xbounds(1):1:xbounds(2));
 set(gca,'fontsize',20);
